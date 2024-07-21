9d65746c7f4a02715e61138d07ec075515a6977b
Olaniyan Oluwafolajimi Opemiposi <folajimiolaniyan@gmail.com>
2024-07-21 20:46:38 +0100

    fixed add products to cart functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,28 +38,36 @@ exports.getSingleProduct = (req, res, next) => {
 }
 
 exports.postSingleItemToCart = (req, res, next) => {
-  const prodId = req.body.productId
-
-  let fetchedCart
-  req.user.getCart().
-    then((cart) => {
-      fetchedCart = cart
-      return cart.getProducts({ where: { id: prodId } })
+  const prodId = req.body.productId;
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
     })
-    .then((products) => {
+    .then(products => {
       let product;
-      if (products.length > 0) product = products[0]
-      let newQuantity = 1;
-      if (product) { }
-
-      return Product.findByPk(prodId)
-        .then(product => {
-          return fetchedCart.addProduct(product, { through: { quantity: newQuantity } })
-        })
-        .catch(err => console.log(err))
+      if (products.length > 0) {
+        product = products[0];
+      }
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
     })
-    .catch(err => { console.log(err) })
-  res.redirect("/")
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getIndex = (req, res, next) => {
@@ -94,12 +102,12 @@ exports.getCart = (req, res, next) => {
     .then((cart) => {
       return cart.getProducts()
         .then((products) => {
-          console.log(products);
-          // res.render('shop/cart', {
-          //   path: '/cart',
-          //   pageTitle: 'Your Cart',
-          //   products: products
-          // })
+          // console.log(products);
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          })
         }).catch(err => { console.log(err); })
     }).catch(err => console.log(err))
 
