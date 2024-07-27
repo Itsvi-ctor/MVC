653e8128bad7bac8f7d819a668dead47910f8434
Olaniyan Oluwafolajimi Opemiposi <folajimiolaniyan@gmail.com>
2024-07-27 19:18:07 +0100

    database mongodb

--- util/database_mongodb.js
+++ util/database_mongodb.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 
 
 const mongoConnect = (cb) => {
-    let connection_string = process.env.mongo_connection_string
-    MongodbClient.connect(`mongodb+srv://folajimi:folajimi@cluster0.6uyxac1.mongodb.net/`).then((client) => {
-        console.log("CONNECTED");
-        cb(client)
-    }).catch(err => { console.log(err); })
+  let connection_string = process.env.mongo_connection_string
+  MongodbClient.connect(`mongodb+srv://folajimi:folajimi@cluster0.6uyxac1.mongodb.net/`).then((client) => {
+    console.log("CONNECTED");
+    cb(client)
+  }).catch(err => { console.log(err); })
 }
 
 module.exports = mongoConnect
